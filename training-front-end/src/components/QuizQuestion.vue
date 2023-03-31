5f53ef1f41bafcc8f1563523870a62e735fde4e6
Mark Meyer <mark@photo-mark.com>
2023-03-30 16:46:44 -0800

    start of styling and behavior for quiz

---
+++ training-front-end/src/components/QuizQuestion.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import { ref } from "vue"
+  const props = defineProps(['question', 'selection'])
+  const emits = defineEmits(['select_answer'])
+  const selected = ref()
+</script>
+<template>
+  
+  
+  <h3 class="">{{ question.question }}</h3>
+
+  <fieldset class="usa-fieldset margin-bottom-2">
+    <div class="usa-radio" v-for="(answer, index) in question.answers">
+      <input
+        class="usa-radio__input"
+        :id="index"
+        type="radio"
+        name="historical-figures"
+        :checked="selection === index"
+        @change="$emit('select_answer', $event.target.value)"
+        :value="index"
+      />
+      <label class="usa-radio__label" :for="index"
+      >{{answer}}</label
+      >
+    </div>
+  </fieldset>
+</template>
