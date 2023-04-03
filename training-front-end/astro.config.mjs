609a34fe24d4ecca91cb5479be5877042986038e
Mark Meyer <mark@photo-mark.com>
2023-04-03 11:01:47 -0800

    add basic structure for static pages

--- training-front-end/astro.config.mjs
+++ training-front-end/astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-
+import mdx from '@astrojs/mdx';
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   base: process.env.BASEURL,
-  integrations: [vue()],
-  outDir: '../_site'
+  integrations: [vue(), mdx()],
+  outDir: '../_site',
 });
--- training-front-end/src/pages/training/purchase_training/index.md
+++
@@ -1,9 +0,0 @@
----
-layout: ../../../layouts/TrainingLayout.astro
-title: Purchase Training for Card / Account Holders and Approving Officials
-quiz_url: /quiz/purchase_training/
----
-
-Training Material Goes Here
-
-[Take the Purchase Training Quiz](../../quiz/purchase_training/)
