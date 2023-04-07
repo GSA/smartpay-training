9470e6c4370cd1a5c96a8442f95b5a651bacfd51
Mark Meyer <mark@photo-mark.com>
2023-04-07 15:56:46 -0800

    add plugin to deal with external links

--- training-front-end/astro.config.mjs
+++ training-front-end/astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import vue from "@astrojs/vue";
+import uswds_links from "./src/plugins/uswds_links"
 
 // https://astro.build/config
 export default defineConfig({
   base: process.env.BASEURL,
   integrations: [vue(), mdx()],
   outDir: '../_site',
+  markdown: {
+    rehypePlugins: [uswds_links]
+  }
 });
---
+++ training-front-end/src/plugins/uswds_links.js
@@ -0,0 +1,21 @@
+/*
+ * The purpose of this is to apply the appropriate class to links
+ * it will add uswds usa-link--external for non gsa.gov links
+ */
+import selector from 'hast-util-select';
+
+const { selectAll } = selector;
+
+export default () => {
+    return node => selectAll('a', node).forEach(node => {
+      const properties = node.properties
+      let domain = new URL(properties.href)
+
+      if (domain.hostname.endsWith('gsa.gov')) {
+        properties.className = 'usa-link'
+      } else {
+        properties.className = 'usa-link usa-link--external'
+      }
+    });
+};
+
