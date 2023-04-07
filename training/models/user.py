1df5de21f7d8bd6bf3ca0456f3b21e84f15c8761
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2023-04-06 18:31:55 -0600

    relationships not currently needed

--- training/models/user.py
+++ training/models/user.py
@@ -1,5 +1,5 @@
 from training.models import Base
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import ForeignKey
 
 
@@ -10,4 +10,3 @@ class User(Base):
     email: Mapped[str] = mapped_column(unique=True, index=True)
     name: Mapped[str] = mapped_column()
     agency_id: Mapped[int] = mapped_column(ForeignKey("agencies.id"))
-    agency = relationship("Agency", back_populates="users")
