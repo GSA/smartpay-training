3ffef2d8b91286a1ddee5ad6b74189c52a5e708a
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2023-04-10 10:43:57 -0600

    deactivate other quizzes of the same type when creating a new quiz

--- training/repositories/quiz.py
+++ training/repositories/quiz.py
@@ -10,10 +10,18 @@ class QuizRepository(BaseRepository[models.Quiz]):
 
     def create(self, quiz: schemas.QuizCreate) -> models.Quiz:
         content_dict = quiz.content.dict()
+
+        # Assign IDs to questions and choices
         for qindex, question in enumerate(content_dict.get("questions", [])):
             question["id"] = qindex
             for cindex, choice in enumerate(question.get("choices", [])):
                 choice["id"] = cindex
+
+        # If this quiz is active, deactivate other quizzes of this type
+        if quiz.active:
+            self._session.query(models.Quiz).filter_by(
+                topic=quiz.topic, audience=quiz.audience
+            ).update({"active": False})
 
         new_quiz = self.save(models.Quiz(
             name=quiz.name,
