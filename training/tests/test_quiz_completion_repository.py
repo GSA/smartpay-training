1382a29740b9636d9c2e1d4e7b8f141dba4dd7cf
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2023-04-14 11:26:02 -0600

    fix quiz completion timestamps

--- training/tests/test_quiz_completion_repository.py
+++ training/tests/test_quiz_completion_repository.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pytest
 from training import schemas
 from training.repositories import QuizCompletionRepository
@@ -19,6 +20,11 @@ def test_create(
     quiz_completion_repo_with_data: QuizCompletionRepository,
     valid_quiz_completion_create: schemas.QuizCompletionCreate,
 ):
+    start = datetime.utcnow()
+    start = start.replace(second=(start.second - 1))
     db_quiz_completion = quiz_completion_repo_with_data.create(valid_quiz_completion_create)
+    end = datetime.utcnow()
+    end = end.replace(second=(end.second + 1))
     assert db_quiz_completion.id
     assert db_quiz_completion.passed
+    assert start <= db_quiz_completion.submit_ts <= end
