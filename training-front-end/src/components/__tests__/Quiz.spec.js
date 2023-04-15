49f3715160cebdbe5e8dbfa91c15df5888e29b55
Mark Meyer <mark@photo-mark.com>
2023-04-14 17:06:55 -0800

    more tests

--- training-front-end/src/components/__tests__/Quiz.spec.js
+++ training-front-end/src/components/__tests__/Quiz.spec.js
@@ -1,56 +1,9 @@
 import { describe, it, expect, afterEach, beforeEach, vi} from 'vitest'
 import { mount, flushPromises } from '@vue/test-utils'
+import sample_quiz  from './fixtures/sample_quiz'
 import Quiz from '../Quiz.vue'
 
-const quiz = {
-  "name": "Test Training for Astro Components and Approving Frameworks",
-  "topic": "Astro",
-  "audience": "Appreciative",
-  "active": true,
-  "content": {
-    "questions": [
-      {
-        "text": "Astro components can include components from other Frameworks like Vue and React",
-        "type": "MultipleChoiceSingleSelect",
-        "choices": [
-          {
-            "text": "True.",
-            "id": 0
-          },
-          {
-            "text": "False.",
-            "id": 1
-          }
-        ],
-        "id": 0
-      },
-      {
-        "text": "Where do content collection markdown files need to be places",
-        "type": "MultipleChoiceSingleSelect",
-        "choices": [
-          {
-            "text": "Under the bed.",
-            "id": 0
-          },
-          {
-            "text": "In the src/content folder.",
-            "id": 1
-          },
-          {
-            "text": "In cold storage with maximum quantum encryption.",
-            "id": 2
-          },
-          {
-            "text": "In the vegetable drawer.",
-            "id": 3
-          }
-        ],
-        "id": 1
-      }
-    ]
-  },
-  "id": 2
-}
+const quiz = sample_quiz
 
 
 describe('Quiz', () => {
