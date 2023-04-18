897c8652a2f97003f6aba21c07d34612c299bcda
Mark Meyer <mark@photo-mark.com>
2023-04-17 17:23:34 -0800

    add linting for astro

--- training-front-end/.eslintrc.cjs
+++ training-front-end/.eslintrc.cjs
@@ -7,11 +7,29 @@ module.exports = {
   extends: [
     // add more generic rulesets here, such as:
     // 'eslint:recommended',
-    'plugin:vue/vue3-recommended',
+    //'plugin:vue/vue3-recommended',
+    //'plugin:astro/recommended'
     // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
   ],
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
-  }
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      plugins: ["astro"],
+      parser: 'astro-eslint-parser',
+      extends: ['plugin:astro/recommended']
+    },
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      extends: ['plugin:vue/vue3-recommended'],
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      },
+    }
+  ],
 }
