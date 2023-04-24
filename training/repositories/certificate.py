7aaef6f50ea1b69afd2ddcccd4a066e9527692c0
weiwang-gsa <wei.wang@gsa.gov>
2023-04-24 15:07:57 -0400

    Added id to UserCertificate, and update get_certificate_by_id return quiz name

--- training/repositories/certificate.py
+++ training/repositories/certificate.py
@@ -9,11 +9,17 @@ class CertificateRepository(BaseRepository[models.QuizCompletion]):
     def __init__(self, session: Session):
         super().__init__(session, models.QuizCompletion)
 
-    def get_certificate_by_id(self, id: int) -> models.QuizCompletion | None:
-        return self._session.query(models.QuizCompletion).filter(models.QuizCompletion.id == id, models.QuizCompletion.passed).first()
+    def get_certificate_by_id(self, id: int) -> UserCertificate | None:
+        
+        result = (self._session.query(models.QuizCompletion.id.label("id"), models.User.id.label("user_id"),
+                                      models.User.name.label("user_name"), models.Quiz.id.label("quiz_id"),
+                                      models.Quiz.name.label("quiz_name"), models.QuizCompletion.submit_ts.label("completion_date")
+                                      ).join(models.User).join(models.Quiz).filter(models.QuizCompletion.passed, models.QuizCompletion.id == id).first())
+        return result
 
     def get_certificates_by_userid(self, user_id: int) -> list[UserCertificate]:
-        results = (self._session.query(models.User.id.label("user_id"), models.User.name.label("user_name"), models.Quiz.id.label("quiz_id"),
+        results = (self._session.query(models.QuizCompletion.id.label("id"), models.User.id.label("user_id"),
+                                       models.User.name.label("user_name"), models.Quiz.id.label("quiz_id"),
                                        models.Quiz.name.label("quiz_name"), models.QuizCompletion.submit_ts.label("completion_date")).join(models.User)
                    .join(models.Quiz).filter(models.QuizCompletion.passed, models.User.id == user_id).all())
         return results
--- training/schemas/user.py
+++ training/schemas/user.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+
 from pydantic import BaseModel
 
 
@@ -19,13 +19,3 @@ class User(UserBase):
     class Config:
         orm_mode = True
 
-
-class UserCertificate(BaseModel):
-    user_id: int
-    user_name: str
-    quiz_id: int
-    quiz_name: str
-    completion_date: datetime
-
-    class Config:
-        orm_mode = True
