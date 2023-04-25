570fbfa467046b27580f6bae2d714002060e8e3c
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2023-04-25 17:08:34 -0600

    split bootstrap into space and app scripts

---
+++ bin/cg-bootstrap-app.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Bootstraps the app in a given space in cloud.gov
+
+if [ -z "$1" ] ; then
+  echo "Usage: $0 SPACE"
+  exit 1
+fi
+
+org="gsa-smartpay"
+space=$1
+
+cf target -o ${org} -s ${space}
+
+# Create the app but don't start it yet
+cf push --no-start --vars-file manifest-vars.${space}.yml
