108b4c2ba4b9c6886bdb1bd475401306e6dcaeb7
Mark Meyer <mark@photo-mark.com>
2023-04-28 15:03:32 -0800

    create sitemap

--- training-front-end/astro.config.mjs
+++ training-front-end/astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import vue from "@astrojs/vue";
-import uswds_links from "./src/plugins/uswds_links"
+import uswds_links from "./src/plugins/uswds_links";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  base: process.env.BASEURL,
-  integrations: [vue(), mdx()],
+  base: '/',
+  site: process.env.BASEURL,
+  integrations: [vue(), mdx(), sitemap()],
   outDir: '../_site',
   markdown: {
     rehypePlugins: [uswds_links]
