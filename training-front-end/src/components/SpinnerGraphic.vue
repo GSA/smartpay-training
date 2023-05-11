d719e0bb13b3039a2a4918db4721bd973ae0bcc8
weiwang-gsa <wei.wang@gsa.gov>
2023-05-11 18:09:38 -0400

    added spinner style and remove it from Vue component

--- training-front-end/src/components/SpinnerGraphic.vue
+++ training-front-end/src/components/SpinnerGraphic.vue
@@ -4,22 +4,4 @@
       <div class="margin-top-1 margin-left-1">Processing...</div>
     </div>
 </template>
- <style is:global>
- .spinner {
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  border: 4px solid #f3f3f3;
-  border-top: 4px solid #3498db;
-  animation: spin 1s infinite linear;
-}
-
-@keyframes spin {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
- </style>
+ 
