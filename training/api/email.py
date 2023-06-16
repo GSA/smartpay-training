4cd90e575c3f7ef06578ab8ff3086650b005f0a9
weiwang-gsa <wei.wang@gsa.gov>
2023-06-16 13:39:02 -0400

    include quiz email  update

--- training/api/email.py
+++ training/api/email.py
@@ -9,33 +9,14 @@ from training.errors import SendEmailError
 
 # We also use jinja template.
 # See: https://sabuhish.github.io/fastapi-mail/example/#using-jinja2-html-templates
-EMAIL_TEMPLATE_TRAINING = Template('''
-<p>Hello $name,</p>
 
-<p>
-Thank you for registering for $training_title. Please confirm the email address
-you submitted is correct by clicking on the link below.
-</p>
-
-<p><a href="$link">$link</a></p>
-
-<p>This link will expire in 24 hours.</p>
-<p>
-If you did not register for this class, you may be receiving this message in error.
-Please disregard this email. If you have any questions or need further assistance,
-email us at gsa_smartpay@gsa.gov.
-</p>
-''')
-
-EMAIL_TEMPLATE_OTHER = Template('''
+EMAIL_TEMPLATE = Template('''
 <p>Hello $name,</p>
 
 <p>
 Click the link below to access your $subject.
 </p>
-
 <p><a href="$link">$link</a></p>
-
 <p>This link will expire in 24 hours.</p>
 <p>
 If you did not submit this request, you may be receiving this message in error.
@@ -51,15 +32,14 @@ def send_email(to_email: EmailStr, name: str, link: str, training_title: str) ->
     if training_title and "certificate" in training_title.lower():
         subject = "GSA SmartPayⓇ training certificate(s)"
         email_subject = "Access your GSA SmartPay training certificate(s)"
-        body = EMAIL_TEMPLATE_OTHER.substitute({"name": name, "link": link, "subject": subject})
     elif training_title and "report" in training_title.lower():
         subject = "GSA SmartPayⓇ reporting information for A/OPCs"
         email_subject = "Access to GSA SmartPay training report"
-        body = EMAIL_TEMPLATE_OTHER.substitute({"name": name, "link": link, "subject": subject})
     else:
-        email_subject = settings.EMAIL_SUBJECT
-        body = EMAIL_TEMPLATE_TRAINING.substitute({"name": name, "link": link, "training_title": training_title})
+        subject = f"GSA SmartPayⓇ {training_title} quiz"
+        email_subject = f"Access GSA SmartPay {training_title} quiz"
 
+    body = EMAIL_TEMPLATE.substitute({"name": name, "link": link, "subject": subject})
     message = EmailMessage()
     message.set_content(body, subtype="html")
     message["Subject"] = email_subject
