03714deb8c49de67b8b53ea8a22e5519c52f78ea
Mark Meyer <mark@photo-mark.com>
2023-07-21 08:38:33 -0800

    no-op change to py file to get tests to run on github

--- training/database/database.py
+++ training/database/database.py
@@ -4,7 +4,7 @@ from training.config import settings
 
 
 # cloud.gov provides the URI in postgres:// format, but SQLAlchemy requires
-# PostgreSQL URIs to use postgresql://
+# PostgreSQL URIs to use postgresql:// 
 db_uri = settings.DB_URI.replace("postgres://", "postgresql://")
 
 engine = create_engine(db_uri, connect_args={'sslmode': 'require'})
