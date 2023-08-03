62e85589904152472baacdce3e81f74fde74e749
Mark Meyer <mark@photo-mark.com>
2023-08-03 12:53:15 -0800

    add a bit more documentation for globalMessages

--- training-front-end/src/components/GlobalMessage.vue
+++ training-front-end/src/components/GlobalMessage.vue
@@ -1,4 +1,14 @@
 <script setup>
+  /**
+   * This compoenent is responsible for displaying alert messages on the front page.
+   * Because this project is built around static pages built with Astro, there is
+   * no global state for the applications. This makes sending messages between pages
+   * challenging. This component depends on the message_manager and session_manager
+   * to set a message in session storage. When the component mounts it reads and 
+   * clears this messages and displays it in the USWDS alert.
+   * 
+   * See stores/message_manager for more details. 
+   */
   import { onMounted, ref } from 'vue'
   import { useStore } from '@nanostores/vue'
   import { message, clearMessage } from '../stores/message_manager.js'
