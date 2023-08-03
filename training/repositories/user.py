eab83ffa6941f9f6aba016e65702655d80b6d68d
weiwang-gsa <wei.wang@gsa.gov>
2023-08-03 14:32:55 -0400

    address email case sensitive issue

--- training/repositories/user.py
+++ training/repositories/user.py
@@ -11,10 +11,10 @@ class UserRepository(BaseRepository[models.User]):
         super().__init__(session, models.User)
 
     def create(self, user: schemas.UserCreate) -> models.User:
-        return self.save(models.User(email=user.email, name=user.name, agency_id=user.agency_id))
+        return self.save(models.User(email=user.email.lower(), name=user.name, agency_id=user.agency_id))
 
     def find_by_email(self, email: str) -> models.User | None:
-        return self._session.query(models.User).filter(models.User.email == email).first()
+        return self._session.query(models.User).filter(models.User.email == email.lower()).first()
 
     def find_by_agency(self, agency_id: int) -> list[models.User]:
         return self._session.query(models.User).filter(models.User.agency_id == agency_id).all()
