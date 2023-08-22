db30e89a60de3f192e5ad7c2dd225d0597106a61
Mark Meyer <mark@photo-mark.com>
2023-08-21 18:28:26 -0800

    add cert tests

---
+++ training/tests/test_api_certificates.py
@@ -0,0 +1,122 @@
+import pytest
+import jwt
+
+
+from unittest.mock import MagicMock
+from fastapi import status
+from fastapi.testclient import TestClient
+from training.api.deps import certificate_repository
+from training.config import settings
+from training.main import app
+from training.schemas import UserCertificate
+from training.services.certificate import Certificate
+
+client = TestClient(app)
+
+
+@pytest.fixture
+def user_cert():
+    return {
+        'id': 1,
+        'user_id': 2,
+        'user_name': "Molly",
+        'agency': 'Freeman Journal',
+        'quiz_id': 100,
+        'quiz_name': "Dublin History",
+        'completion_date': '2023-08-21T22:59:36'
+    }
+
+
+@pytest.fixture
+def fake_cert_repo():
+    mock = MagicMock()
+    app.dependency_overrides[certificate_repository] = lambda: mock
+    yield mock
+    app.dependency_overrides = {}
+
+
+@pytest.fixture
+def fake_cert_service_repo():
+    mock = MagicMock()
+    app.dependency_overrides[Certificate] = lambda: mock
+    yield mock
+    app.dependency_overrides = {}
+
+
+@pytest.fixture
+def goodJWT():
+    return jwt.encode({'id': 1}, settings.JWT_SECRET, algorithm="HS256")
+
+
+class TestCertificateAPI:
+    def test_get_certificates_no_auth(self, fake_cert_repo):
+        response = client.get(
+            "/api/v1/certificates"
+        )
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_get_no_certificates(self, fake_cert_repo, goodJWT):
+        fake_cert_repo.get_certificates_by_userid.return_value = None
+        response = client.get(
+            "/api/v1/certificates",
+            headers={"Authorization": f"Bearer {goodJWT}"}
+        )
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_gets_certificates_by_id(self, fake_cert_repo, goodJWT):
+        fake_cert_repo.get_certificates_by_userid.return_value = None
+        client.get(
+            "/api/v1/certificates",
+            headers={"Authorization": f"Bearer {goodJWT}"}
+        )
+        fake_cert_repo.get_certificates_by_userid.assert_called_once_with(1)
+
+    def test_gets_certificates(self, fake_cert_repo, goodJWT, user_cert):
+        cert = UserCertificate.model_validate(user_cert)
+        fake_cert_repo.get_certificates_by_userid.return_value = [cert]
+        response = client.get(
+            "/api/v1/certificates",
+            headers={"Authorization": f"Bearer {goodJWT}"}
+        )
+        assert response.json() == [user_cert]
+
+    def test_get_specific_certificate_not_found(self, fake_cert_repo, goodJWT):
+        fake_cert_repo.get_certificate_by_id.return_value = None
+        response = client.post(
+            "/api/v1/certificate/2",
+            data={"jwtToken": goodJWT}
+        )
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+        fake_cert_repo.get_certificate_by_id.assert_called_once_with(2)
+
+    def test_get_specific_certificate_wrong_user(self, fake_cert_repo, goodJWT, user_cert):
+        cert = UserCertificate.model_validate(user_cert)
+        fake_cert_repo.get_certificate_by_id.return_value = cert
+        response = client.post(
+            "/api/v1/certificate/2",
+            data={"jwtToken": goodJWT}
+        )
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_get_specific_certificate(self, fake_cert_repo, goodJWT, user_cert, fake_cert_service_repo):
+        user_cert['user_id'] = 1
+        cert = UserCertificate.model_validate(user_cert)
+        fake_cert_repo.get_certificate_by_id.return_value = cert
+        fake_cert_service_repo.generate_pdf.return_value = b'some bytes'
+
+        response = client.post(
+            "/api/v1/certificate/2",
+            data={"jwtToken": goodJWT}
+        )
+        fake_cert_service_repo.generate_pdf.assert_called_once_with(
+            cert.quiz_name,
+            cert.user_name,
+            cert.agency,
+            cert.completion_date
+        )
+        assert response.status_code == status.HTTP_200_OK
+        print(dir(response))
+        print(response.headers)
+        assert response.headers['content-type'] == 'application/pdf'
+        assert response.headers['content-disposition'] == 'attachment; filename="SmartPayTraining.pdf"'
+        assert response.text == "some bytes"
