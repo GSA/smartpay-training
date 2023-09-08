fd57cb929245c567418acd734cac8c1cf9ee49e4
weiwang-gsa <wei.wang@gsa.gov>
2023-09-08 16:09:46 -0400

    update content to be consistent per 116

--- training/api/email.py
+++ training/api/email.py
@@ -29,13 +29,13 @@ email us at gsa_smartpay@gsa.gov.
 def send_email(to_email: EmailStr, name: str, link: str, training_title: str) -> None:
 
     if training_title and "certificate" in training_title.lower():
-        subject = "GSA SmartPayⓇ training certificate(s)"
+        subject = "GSA SmartPay® training certificate(s)"
         email_subject = "Access your GSA SmartPay training certificate(s)"
     elif training_title and "report" in training_title.lower():
-        subject = "GSA SmartPayⓇ reporting information for A/OPCs"
+        subject = "GSA SmartPay® reporting information for A/OPCs"
         email_subject = "Access to GSA SmartPay training report"
     else:
-        subject = f"GSA SmartPayⓇ {training_title} quiz"
+        subject = f"GSA SmartPay® {training_title} quiz"
         email_subject = f"Access GSA SmartPay {training_title} quiz"
 
     body = EMAIL_TEMPLATE.substitute({"name": name, "link": link, "subject": subject})
