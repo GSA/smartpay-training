8bcb4b645c7ae08a2d6c63fc654486fb0753f16a
Mark Meyer <mark@photo-mark.com>
2023-10-05 07:34:23 -0800

    fix typo in cors domain

--- training/main.py
+++ training/main.py
@@ -14,7 +14,7 @@ origins = [
     "http://localhost:3000",
     "https://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://training.smartpay.gov",
+    "https://training.smartpay.gsa.gov",
     "https://federalist-2e11f2c8-970f-44f5-acc8-b47ef6c741ad.sites.pages.cloud.gov"
 ]
 
