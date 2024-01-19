a40a9a0342f3c20732b1d11e48589a11cdf32d12
John Labbate <j.labbate@kunzleigh.com>
2024-01-19 13:18:00 -0500

    Update to astro 4 and fix lint errors

--- training-front-end/.eslintrc.cjs
+++ training-front-end/.eslintrc.cjs
@@ -44,4 +44,5 @@ module.exports = {
       }
     }
   ],
+  root:true
 }
--- training-front-end/src/plugins/uswds_links.js
+++ training-front-end/src/plugins/uswds_links.js
@@ -4,26 +4,28 @@
  */
 import selector from 'hast-util-select';
 
-const { selectAll } = selector;
-
 export default () => {
-    return node => selectAll('a', node).forEach(node => {
-      const properties = node.properties
-      properties.className = 'usa-link'
+    return node => {
+      if (node){
+        selector.selectAll('a', node).forEach(node => {
+        if(!node.properties){
+          return;
+        }
+        const properties = node.properties
+        properties.className = 'usa-link'
 
-      let domain;
-      try { 
-        domain = new URL(properties.href)
-      } catch(e) {
-        // Exceptions will be raised with relative links
-        // these will all be local, so ignore
-        return
-      }
-    
-      if (! domain.hostname.endsWith('gsa.gov')) {
-        properties.className += ' usa-link--external'
-      }
-    
-    });
-};
-
+        let domain;
+        try { 
+          domain = new URL(properties.href)
+        } catch(e) {
+          // Exceptions will be raised with relative links
+          // these will all be local, so ignore
+          return
+        }
+      
+        if (! domain.hostname.endsWith('gsa.gov')) {
+          properties.className += ' usa-link--external'
+        }
+      });
+  }};
+}
