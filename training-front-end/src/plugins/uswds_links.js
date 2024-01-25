ff09693b1d47fd687cb58e1ec60c11840dbfc48e
John Labbate <j.labbate@kunzleigh.com>
2024-01-25 09:52:40 -0500

    revert unneeded uswds_links file changes.

--- training-front-end/src/plugins/uswds_links.js
+++ training-front-end/src/plugins/uswds_links.js
@@ -4,28 +4,25 @@
  */
 import selector from 'hast-util-select';
 
-export default () => {
-    return node => {
-      if (node){
-        selector.selectAll('a', node).forEach(node => {
-        if(!node.properties){
-          return;
-        }
-        const properties = node.properties
-        properties.className = 'usa-link'
+const { selectAll } = selector;
 
-        let domain;
-        try { 
-          domain = new URL(properties.href)
-        } catch(e) {
-          // Exceptions will be raised with relative links
-          // these will all be local, so ignore
-          return
-        }
-      
-        if (! domain.hostname.endsWith('gsa.gov')) {
-          properties.className += ' usa-link--external'
-        }
-      });
-  }};
-}
+export default () => {
+    return node => selectAll('a', node).forEach(node => {
+      const properties = node.properties
+      properties.className = 'usa-link'
+
+      let domain;
+      try { 
+        domain = new URL(properties.href)
+      } catch(e) {
+        // Exceptions will be raised with relative links
+        // these will all be local, so ignore
+        return
+      }
+
+      if (! domain.hostname.endsWith('gsa.gov')) {
+        properties.className += ' usa-link--external'
+      }
+
+    });
+};
