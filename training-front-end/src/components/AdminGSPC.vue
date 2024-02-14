485573af99fff57335dcbb5e8841a57a49c42142
John Labbate <j.labbate@kunzleigh.com>
2024-02-14 15:44:50 -0500

    remove extra "s"

---
+++ training-front-end/src/components/AdminGSPC.vue
@@ -0,0 +1,61 @@
+<script setup>
+  import USWDSAlert from './USWDSAlert.vue'
+</script>
+
+<template>
+  <div class="padding-top-4 padding-bottom-4 grid-container">
+    <USWDSAlert
+      v-if="error"
+      status="error"
+      :heading="error.name"
+    >
+      {{ error.message }}
+    </USWDSAlert>
+  </div>
+  <div class="padding-top-4 padding-bottom-4 grid-container">
+    <h2>GSPC Send Email Notifications</h2>
+    <p>
+      Content here duis vulputate pellentesque commodo. Nullam congue nibh a diam porta, at tincidunt mi pellentesque. Curabitur at mollis erat. Pellentesque ultricies libero sem, sed ornare nisl faucibus sit amet. In vel dictum tortor. Nullam iaculis efficitur ipsum. Quisque pharetra euismod augue et placerat.
+    </p>
+    <p>
+      Duis congue, eros eu volutpat suscipit, odio dui varius nisi, id bibendum ipsum nisi at libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer felis velit, sollicitudin a leo sagittis, iaculis efficitur turpis. Vivamus id mi sed sapien facilisis consequat pulvinar sed orci. Morbi tellus quam, consectetur in aliquet eu, auctor id urna.
+    </p>
+    <form 
+      class="usa-search"
+      role="search"
+      @submit.prevent="search"
+    >
+      <label 
+        class="usa-label" 
+        for="email-addresses-textarea"
+      >
+        Email Addresses of GSA SmartPay Forum Attendees 
+        <span class="text-secondary-dark">(*Required)</span>
+      </label>
+      <span v-if="validator.$error">
+        <span
+          v-for="error in validator.$errors"
+          :id="error_id"
+          :key="error.$property"
+          class="usa-error-message"
+          role="alert"
+        >
+          {{ error.$message }}
+        </span>
+      </span>
+      <textarea
+        id="email-addresses-textarea"
+        class="usa-textarea usa-input tablet:grid-col-8"
+        name="email-addresses-textarea"
+      />
+      <button 
+        class="usa-button"
+        type="submit"
+      >
+        <span class="usa-search__submit-text">
+          Search
+        </span>
+      </button>
+    </form>
+  </div>
+</template>
---
+++ training-front-end/src/components/ValidatedTextArea.vue
@@ -0,0 +1,63 @@
+<script setup>
+  import {computed} from 'vue'
+  const props = defineProps({
+    'modelValue': {
+      type: String,
+      required: false,
+      default: undefined
+    },
+    'validator': {
+      type: Object,
+      required: true
+    },
+    'name': {
+      type: String,
+      required: true
+    },
+    'label': {
+      type: String,
+      required: true
+    },
+    'readonly': {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  })
+
+  defineEmits(['update:modelValue'])
+  var error_id = computed(() => props.name + '-input-error-message')
+</script>
+
+<template>
+  <div
+    class="usa-form-group"
+    :class="{ 'usa-form-group--error':validator.$error}"
+  >
+    <label
+      class="usa-label"
+      :for="name"
+    >{{ label }} <span class="text-secondary-dark">(*Required)</span></label>
+    <span v-if="validator.$error">
+      <span
+        v-for="error in validator.$errors"
+        :id="error_id"
+        :key="error.$property"
+        class="usa-error-message"
+        role="alert"
+      >
+        {{ error.$message }}
+      </span>
+    </span>
+    <textarea
+      :id="name"
+      class="usa-input usa-input tablet:grid-col-8"
+      :class="{ 'usa-input--error':validator.$error, 'error-focus': validator.$error }"
+      :name="name"
+      :value="modelValue"
+      :aria-describedby="validator.$error? error_id: null"
+      :readonly="readonly"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>    
+</template>
