b09caa33654a37235eb6c72133e945363e24ed19
John Labbate <j.labbate@kunzleigh.com>
2024-03-06 08:40:40 -0500

    removed dependancy on sending email in test.

--- training/tests/test_api_gspc.py
+++ training/tests/test_api_gspc.py
@@ -55,15 +55,18 @@ def standard_payload():
 
 class TestGspc:
     @patch('training.config.settings', 'JWT_SECRET', 'super_secret')
-    def test_gspc_invite_success(self, goodJWT, standard_payload, fake_gspc_invite_repo):
+    @patch('training.api.api_v1.gspc.send_gspc_invite_email')
+    def test_gspc_invite_success(self, send_gspc_invite_email, goodJWT, standard_payload, fake_gspc_invite_repo):
         '''Given 2 valid emails it should call the db create method for each'''
+        send_gspc_invite_email.return_value = "email response"
         response = post_gspc_invite(standard_payload, goodJWT)
-
+        
         assert response.status_code == 200
         assert fake_gspc_invite_repo.create.call_count == 2
 
     @patch('training.config.settings', 'JWT_SECRET', 'super_secret')
-    def test_gspc_invite_parses_valid_emails(self, goodJWT, standard_payload, fake_gspc_invite_repo):
+    @patch('training.api.api_v1.gspc.send_gspc_invite_email')
+    def test_gspc_invite_parses_valid_emails(self, send_gspc_invite_email, goodJWT, standard_payload, fake_gspc_invite_repo):
         '''Given 2 valid emails in a list of 4 it should return a list of 2 valid emails'''
         response = post_gspc_invite(standard_payload, goodJWT)
 
@@ -73,7 +76,8 @@ class TestGspc:
         assert "ValidEmail2@test.com" in emailList
 
     @patch('training.config.settings', 'JWT_SECRET', 'super_secret')
-    def test_gspc_invite_parses_invalid_emails(self, goodJWT, standard_payload, fake_gspc_invite_repo):
+    @patch('training.api.api_v1.gspc.send_gspc_invite_email')
+    def test_gspc_invite_parses_invalid_emails(self, send_gspc_invite_email, goodJWT, standard_payload, fake_gspc_invite_repo):
         '''Given 2 invalid emails in a list of 4 it should return a list of 2 invalid emails'''
         response = post_gspc_invite(standard_payload, goodJWT)
 
@@ -83,6 +87,7 @@ class TestGspc:
         assert "@invalidEmail.2" in emailList
 
     @patch('training.config.settings', 'JWT_SECRET', 'super_secret')
+    @patch('training.api.api_v1.gspc.send_gspc_invite_email')
     def test_gspc_invite_sends_emails_to_valid_emails(self, goodJWT, standard_payload, fake_gspc_invite_repo):
         '''Given 2 valid emails send 2 invite emails'''
         with patch('training.api.api_v1.gspc.send_gspc_invite_email') as mock_send_email:
@@ -90,7 +95,8 @@ class TestGspc:
             assert mock_send_email.call_count == 2
 
     @patch('training.config.settings', 'JWT_SECRET', 'super_secret')
-    def test_gspc_invite_loggs_emails(self, goodJWT, standard_payload, fake_gspc_invite_repo):
+    @patch('training.api.api_v1.gspc.send_gspc_invite_email')
+    def test_gspc_invite_loggs_emails(self, send_gspc_invite_email, goodJWT, standard_payload, fake_gspc_invite_repo):
         '''Given 2 valid emails logger logs emails sent'''
         with patch('training.api.api_v1.gspc.logging') as logger:
             post_gspc_invite(standard_payload, goodJWT)
