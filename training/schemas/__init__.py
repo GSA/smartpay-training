535adcd76e687e289857907d318e5971a4624f90
Dan Felder <30899941+felder101@users.noreply.github.com>
2024-05-31 09:21:23 -0400

    Merging Sprint 31 stories (#571)
    BUG: Update the GSPC URL #562
    User can view GSPC Certificates on Access Past Certificates #556
    Question 14: Purchase A/OPC Training: Deactivation and reactivation #563
    Question 3. Purchase Card/Account Holder Training Quiz #551
    Question 23: Card/Account Holders and Approving Officials Purchase Training Quiz #462

--- training/schemas/__init__.py
+++ training/schemas/__init__.py
@@ -13,7 +13,7 @@ from .quiz import Quiz, QuizCreate, QuizPublic, QuizTopic, QuizAudience
 from .quiz_submission import QuizSubmission
 from .quiz_grade import QuizGrade
 from .quiz_completion import QuizCompletion, QuizCompletionCreate
-from .user_certificate import UserCertificate
+from .user_certificate import UserCertificate, CertificateType, CertificateListValue
 from .user_x_role import UserXRole
 from .report_user_x_agency import ReportUserXAgency
 from .role import Role, RoleCreate
