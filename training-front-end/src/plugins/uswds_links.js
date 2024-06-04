80bc62f7206214e6b9b188d84b136a4f684dc7fc
Dan Felder <d.felder@kunzleigh.com>
2024-06-04 13:05:16 -0400

    Fixed plugin for application link processing

--- training-front-end/src/plugins/uswds_links.js
+++ training-front-end/src/plugins/uswds_links.js
@@ -2,26 +2,27 @@
  * The purpose of this is to apply the appropriate class to links
  * it will add uswds usa-link--external for non gsa.gov links
  */
-import selectAll from 'hast-util-select';
+import {selectAll} from 'hast-util-select';
 
-export default () => {
-    return node => selectAll('a', node).forEach(node => {
-      const properties = node.properties
-      properties.className = 'usa-link'
+export function processLinksPlugin() {
+    return function (node) {
+      selectAll('a', node).forEach(node => {
+        const properties = node.properties
+        properties.className = 'usa-link'
 
-      let domain;
-      try { 
-        domain = new URL(properties.href)
-      } catch(e) {
-        // Exceptions will be raised with relative links
-        // these will all be local, so ignore
-        return
-      }
-    
-      if (! domain.hostname.endsWith('gsa.gov')) {
-        properties.className += ' usa-link--external'
-      }
-    
-    });
-};
+        let domain;
+        try {
+          domain = new URL(properties.href)
+        } catch(e) {
+          // Exceptions will be raised with relative links
+          // these will all be local, so ignore
+          return
+        }
 
+        if (! domain.hostname.endsWith('gsa.gov')) {
+          properties.className += ' usa-link--external'
+        }
+
+      });
+    } 
+}
