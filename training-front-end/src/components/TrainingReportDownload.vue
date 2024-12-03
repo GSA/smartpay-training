6a1c433c11ac1cdce8598e403b1e20317f4777ec
Dan Felder <d.felder@kunzleigh.com>
2024-12-03 09:33:38 -0500

    Add back-end logic for downloading report with filters. Issue exists with the API call.

--- training-front-end/src/components/TrainingReportDownload.vue
+++ training-front-end/src/components/TrainingReportDownload.vue
@@ -1,14 +1,21 @@
 <script setup>
-import {computed, onMounted, reactive, ref, watch} from "vue"
+import {computed, reactive, ref, watch} from "vue"
 import { useStore } from '@nanostores/vue'
 import { useVuelidate } from "@vuelidate/core";
 import { profile } from '../stores/user' 
 import USWDSAlert from './USWDSAlert.vue'
 import ValidatedDateRangePicker from "./form-components/ValidatedDateRangePicker.vue";
-import ValidatedCheckboxGroup from "@components/form-components/ValidatedCheckboxGroup.vue";
-import USWDSComboBox from "@components/form-components/USWDSComboBox.vue";
+import ValidatedCheckboxGroup from "./form-components/ValidatedCheckboxGroup.vue";
+import USWDSComboBox from "./form-components/USWDSComboBox.vue";
 import {agencyList, bureauList, setSelectedAgencyId} from "../stores/agencies.js";
+import ReportRepository from "./ReportRepository.vue";
+import ReportUtilities from "./ReportUtilities.vue";
+import SpinnerGraphic from "@components/SpinnerGraphic.vue";
 
+const error = ref()
+const showSuccessMessage = ref(false)
+const isLoading = ref(false)
+const showSpinner = ref(false)
 const user = useStore(profile)
 const isReportUser = computed(() => user.value.roles.includes('Report'))
 const agency_options = useStore(agencyList)
@@ -25,9 +32,6 @@ filteredBureauOptions = computed(() => {
   return bureaus.value.filter((bureau) => report_agencies.map(x => x.id).includes(bureau.id))
 })
 
-const base_url = import.meta.env.PUBLIC_API_BASE_URL
-const report_url = `${base_url}/api/v1/users/download-user-quiz-completion-report`
-
 //Properties 
 const user_input = reactive({
   agency_id: undefined,
@@ -41,6 +45,9 @@ watch(() => user_input.agency_id, async() => {
   user_input.bureau_id = undefined
 })
 
+function setError(event){
+  error.value = event
+}
 
 const validation_info = {
   agency_id: {},
@@ -58,6 +65,39 @@ const quiz_names_options = [
 ];
 
 const v_all_info$ = useVuelidate(validation_info, user_input)
+
+//format dates from uswds standard to the format needed for backend 
+const formatDateToYYYYMMDD = (dates) => {
+  return dates ? dates.map(date => (date ? new Date(date).toISOString().split('T')[0] : null)) : [];
+};
+
+async function downloadReport() {
+  isLoading.value = true
+  showSpinner.value = true
+  error.value = undefined
+  showSuccessMessage.value = false
+
+  try {
+    const dates = formatDateToYYYYMMDD(user_input.completion_date_range)
+    const model = {
+      'agency_id': user_input.agency_id || null,
+      'bureau_id': user_input.bureau_id || null,
+      'completion_date_start': dates[0] || null,
+      'completion_date_end': dates[1] || null,
+      'quiz_names': user_input.quiz_names || null
+    }
+    let response = await ReportRepository.downloadTrainingCompletionReport(model)
+    const blob = await response.blob();
+    await ReportUtilities.downloadBlobAsFile(blob, 'SmartPayTrainingReport.csv')
+    showSuccessMessage.value = true
+
+  } catch (error) {
+    setError(error)
+  }
+
+  isLoading.value = false
+  showSpinner.value = false
+}
 
 </script>
 <template>
@@ -78,6 +118,7 @@ const v_all_info$ = useVuelidate(validation_info, user_input)
           ref="form"
           class="usa-form usa-form--large margin-bottom-3"
           data-test="report-form"
+          @submit.prevent="downloadReport"
       >
         <ValidatedDateRangePicker
             v-model="user_input.completion_date_range"
@@ -110,7 +151,47 @@ const v_all_info$ = useVuelidate(validation_info, user_input)
             label="Sub-agency, organization, or bureau"
             name="Bureau"
         />
-
+        <input
+            class="usa-button"
+            type="submit"
+            value="Submit"
+            :disabled="isLoading"
+            data-test="submit"
+        >
+        <div>
+          <USWDSAlert
+              v-if="error"
+              status="error"
+              class="usa-alert--slim"
+              :has-heading="false"
+          >
+            {{ error }}
+          </USWDSAlert>
+          <USWDSAlert
+              v-if="showSuccessMessage"
+              status="success"
+              class="usa-alert--slim"
+              :has-heading="false"
+          >
+            Report has been generated.
+          </USWDSAlert>
+        </div>
+        <div class="grid-row grid-gap margin-top-3">
+          <!--display spinner along with submit button in one row for desktop-->
+          <div
+              v-if="showSpinner"
+              class="display-none tablet:display-block tablet:grid-col-1 tablet:padding-top-3 tablet:margin-left-neg-1"
+          >
+            <SpinnerGraphic />
+          </div>
+        </div>
+        <!--display spinner under submit button for mobile view-->
+        <div
+            v-if="showSpinner"
+            class="tablet:display-none margin-top-1 text-center"
+        >
+          <SpinnerGraphic />
+        </div>
       </form>
     </div>
 
