5e093bbdfa915efeebc9e1dc2f3f2eba1873ae19
John Labbate <j.labbate@kunzleigh.com>
2025-03-17 09:57:59 -0400

    lint errors and bugfix.

--- training-front-end/src/components/AdminGSPC.vue
+++ training-front-end/src/components/AdminGSPC.vue
@@ -120,6 +120,29 @@
 
     endLoading()
   }
+
+  async function handlePaste(){
+      // Let the paste happen normally, then format automatically
+      setTimeout(() => {
+        formatTextArea();
+      }, 100);
+  }
+
+  async function formatTextArea(){
+    if (!(user_input.emailAddresses)) return;
+    
+    // Detect if this looks like an Excel paste
+    // Excel pastes often have repeated items with no separators or just spaces
+    const text = user_input.emailAddresses.trim();
+    const lines = text.split(/\s+/);
+    
+    // If it doesn't look like an Excel paste, just keep the original
+    if (lines.length > 1) {
+      user_input.emailAddresses = lines
+          .filter(line => line.trim() !== '')
+          .join(', ');
+    }
+  }
 </script>
 <template>
   <div class="padding-top-4 padding-bottom-4 grid-container">
@@ -144,38 +167,80 @@
               date of the GSA SmartPay Training Forum. This form will verify the entered email addresses and notify you
               if any are invalid.
             </p>
-            <form ref="form" class="usa-form usa-form--large margin-bottom-3" data-test="gspc-form"
-              @submit.prevent="submitGspcInvites">
-              <ValidatedTextArea v-model="user_input.emailAddresses" client:load :validator="v_all_info$.emailAddresses"
-                label="Email Addresses of GSA SmartPay Forum Attendees" name="email-list" :required="true" />
-              <ValidatedMemorableDatepicker v-model="user_input.certificationExpirationDate" client:load
-                :validator="v_all_info$.certificationExpirationDate" label="Certification Expiration Date"
-                name="certification-expiration-date" hint-text="For example: January 19 2000" :required="true" />
+            <form
+              ref="form"
+              class="usa-form usa-form--large margin-bottom-3"
+              data-test="gspc-form"
+              @submit.prevent="submitGspcInvites"
+            >
+              <ValidatedTextArea
+                v-model="user_input.emailAddresses"
+                client:load
+                :validator="v_all_info$.emailAddresses"
+                label="Email Addresses of GSA SmartPay Forum Attendees"
+                name="email-list"
+                :required="true"
+                @update:paste="handlePaste"
+              />
+              <ValidatedMemorableDatepicker
+                v-model="user_input.certificationExpirationDate"
+                client:load
+                :validator="v_all_info$.certificationExpirationDate"
+                label="Certification Expiration Date"
+                name="certification-expiration-date"
+                hint-text="For example: January 19 2000"
+                :required="true"
+              />
               <div>
-                <USWDSAlert v-if="showFailedMessage" status="error" class="usa-alert--slim" :has-heading="false">
+                <USWDSAlert
+                  v-if="showFailedMessage"
+                  status="error"
+                  class="usa-alert--slim"
+                  :has-heading="false"
+                >
                   Emails failed to send to: {{ failedEmailList }}
                 </USWDSAlert>
-                <USWDSAlert v-if="showSuccessMessage" status="success" class="usa-alert--slim" :has-heading="false">
+                <USWDSAlert
+                  v-if="showSuccessMessage"
+                  status="success"
+                  class="usa-alert--slim"
+                  :has-heading="false"
+                >
                   Emails sending to {{ successCount }} people.
                 </USWDSAlert>
               </div>
               <div class="grid-row grid-gap margin-top-3">
                 <div class="grid-col">
-                  <input class="usa-button" type="submit" value="Send Invitations" :disabled="isLoading"
-                    data-test="submit">
-                  <button id="cancel" type="button" class="usa-button usa-button--outline" :disabled="isLoading"
-                    @click="cancel">
+                  <input
+                    class="usa-button"
+                    type="submit"
+                    value="Send Invitations"
+                    :disabled="isLoading"
+                    data-test="submit"
+                  >
+                  <button
+                    id="cancel"
+                    type="button"
+                    class="usa-button usa-button--outline"
+                    :disabled="isLoading"
+                    @click="cancel"
+                  >
                     Cancel
                   </button>
                 </div>
                 <!--display spinner along with submit button in one row for desktop-->
-                <div v-if="showSpinner"
-                  class="display-none tablet:display-block tablet:grid-col-1 tablet:padding-top-3 tablet:margin-left-neg-1">
+                <div
+                  v-if="showSpinner"
+                  class="display-none tablet:display-block tablet:grid-col-1 tablet:padding-top-3 tablet:margin-left-neg-1"
+                >
                   <SpinnerGraphic />
                 </div>
               </div>
               <!--display spinner under submit button for mobile view-->
-              <div v-if="showSpinner" class="tablet:display-none margin-top-1 text-center">
+              <div
+                v-if="showSpinner"
+                class="tablet:display-none margin-top-1 text-center"
+              >
                 <SpinnerGraphic />
               </div>
             </form>
@@ -192,40 +257,53 @@
           <div class="usa-card__body">
             <p>
               Sends out follow up GSPC invite emails to users who have
-            <ul>
-              <li>Received the original GSPC invite within the last 6 months</li>
-              <li>Hasn't already completed the GSPC survey</li>
-              <li>Hasn't already received both follow up notifications</li>
-              <li>Hasn't received the last GSPC email within 12 hours</li>
-            </ul>
+              <ul>
+                <li>Received the original GSPC invite within the last 6 months</li>
+                <li>Hasn't already completed the GSPC survey</li>
+                <li>Hasn't already received both follow up notifications</li>
+                <li>Hasn't received the last GSPC email within 12 hours</li>
+              </ul>
             </p>
             <p>
               The system will send the subsequent needed notification based on the last one received by the user.
             </p>
             <div>
-              <USWDSAlert v-if="showFollowUpSuccessMessage" status="success" class="usa-alert--slim"
-                :has-heading="false">
+              <USWDSAlert
+                v-if="showFollowUpSuccessMessage"
+                status="success"
+                class="usa-alert--slim"
+                :has-heading="false"
+              >
                 Sending out GSPC follow up emails.
               </USWDSAlert>
             </div>
             <div class="grid-row grid-gap margin-top-3">
               <div class="grid-col">
-                <button id="send-out-follow-ups" type="button" class="usa-button" :disabled="isLoading"
-                  @click="sendGspcFollowUps">
+                <button
+                  id="send-out-follow-ups"
+                  type="button"
+                  class="usa-button"
+                  :disabled="isLoading"
+                  @click="sendGspcFollowUps"
+                >
                   Send Out Follow Ups
                 </button>
               </div>
               <!--display spinner along with submit button in one row for desktop-->
-              <div v-if="showSpinner"
-                class="display-none tablet:display-block tablet:grid-col-1 tablet:padding-top-3 tablet:margin-left-neg-1">
+              <div
+                v-if="showSpinner"
+                class="display-none tablet:display-block tablet:grid-col-1 tablet:padding-top-3 tablet:margin-left-neg-1"
+              >
                 <SpinnerGraphic />
               </div>
             </div>
-            <div v-if="showSpinner" class="tablet:display-none margin-top-1 text-center">
+            <div
+              v-if="showSpinner"
+              class="tablet:display-none margin-top-1 text-center"
+            >
               <SpinnerGraphic />
             </div>
           </div>
-
         </div>
       </li>
     </ul>
