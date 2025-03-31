760507a8dd7a378d406de9cab70f8d25241cac1a
Dan Felder <d.felder@kunzleigh.com>
2025-03-31 15:24:44 -0400

    Add test for download admin user roles report API

--- training/api/api_v1/users.py
+++ training/api/api_v1/users.py
@@ -191,7 +191,7 @@ def download_admin_users_roles_report_csv(
     writer = csv.writer(output)
 
     # header row
-    writer.writerow(['Full Name', 'Email Address', 'Assigned Agency', 'Assigned Bureau', 'Admin?', 'Report?', 'Report Agency', 'Report Bureau(s)'])
+    writer.writerow(['Full Name', 'Email Address', 'Assigned Agency', 'Assigned Bureau', 'Admin', 'Report', 'Report Agency', 'Report Bureau(s)'])
     for item in results:
         # data row
         writer.writerow([item.name, item.email, item.assignedAgency, item.assignedBureau, item.adminRole, item.reportRole, item.reportAgency, item.reportBureau])  # noqa 501
