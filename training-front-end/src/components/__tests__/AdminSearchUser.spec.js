43f607318c8c324f64b116b283262e32bac78e4b
Dan Felder <d.felder@kunzleigh.com>
2025-06-17 12:57:53 -0400

    Fixed unit test

--- training-front-end/src/components/__tests__/AdminSearchUser.spec.js
+++ training-front-end/src/components/__tests__/AdminSearchUser.spec.js
@@ -16,12 +16,14 @@ describe('AdminAgencySelect', async () => {
   })
   
   it('displays a search input', async () => {
+    profile.set({name:"Amelia Sedley", jwt:"some-token-value", roles:["Admin"]})
     let wrapper = await mount(AdminSearchUserVue)
     const searchInput = wrapper.find('input[id="search-field"]')
     expect(searchInput.exists()).toBe(true)
   })
 
   it('displays a search results', async () => {
+    profile.set({name:"Amelia Sedley", jwt:"some-token-value", roles:["Admin"]})
     const fetchSpy = vi.spyOn(global, 'fetch').mockImplementation(() => {
       return Promise.resolve({ok: true, status:200, json: () => Promise.resolve({'total_count': 2, 'users':users}) })
     })
@@ -41,7 +43,7 @@ describe('AdminAgencySelect', async () => {
   })
 
   it('updates with API when child component emits data', async () => {
-    profile.set({name:"Ortho Stice", jwt:"some-token-value"})
+    profile.set({name:"Ortho Stice", jwt:"some-token-value", roles:["Admin"]})
     let fetchSpy = vi.spyOn(global, 'fetch').mockImplementation(() => {
       return Promise.resolve({ok: true, status:200, json: () => Promise.resolve({'total_count': 2, 'users':users}) })
     })
@@ -80,6 +82,7 @@ describe('AdminAgencySelect', async () => {
   })
 
   it('allows user to cancel update', async () => {
+    profile.set({name:"Amelia Sedley", jwt:"some-token-value", roles:["Admin"]})
     vi.spyOn(global, 'fetch').mockImplementation(() => {
       return Promise.resolve({ok: true, status:200, json: () => Promise.resolve({'total_count': 2, 'users':users}) })
     })
@@ -105,6 +108,7 @@ describe('AdminAgencySelect', async () => {
   })
 
   it('updates page when pagination emits', async () => {
+    profile.set({name:"Amelia Sedley", jwt:"some-token-value", roles:["Admin"]})
     let fetchSpy = vi.spyOn(global, 'fetch').mockImplementation(() => {
       return Promise.resolve({ok: true, status:200, json: () => Promise.resolve({'total_count': 26, 'users':users}) })
     })
@@ -124,6 +128,7 @@ describe('AdminAgencySelect', async () => {
   })
 
   it('displays no results message', async () => {
+    profile.set({name:"Amelia Sedley", jwt:"some-token-value", roles:["Admin"]})
     vi.spyOn(global, 'fetch').mockImplementation(() => {
       return Promise.resolve({ok: true, status:200, json: () => Promise.resolve({'total_count': 0, 'users':[]}) })
     })
@@ -140,6 +145,7 @@ describe('AdminAgencySelect', async () => {
   })
 
   it('displays error with server message on non-2xx response', async () => {
+    profile.set({name:"Amelia Sedley", jwt:"some-token-value", roles:["Admin"]})
     vi.spyOn(global, 'fetch').mockImplementation(() => {
       return Promise.resolve({ok: false, status:403, text: () => Promise.resolve('Forbidden') })
     })
@@ -155,6 +161,7 @@ describe('AdminAgencySelect', async () => {
   })
 
   it('displays error on server error', async () => {
+    profile.set({name:"Amelia Sedley", jwt:"some-token-value", roles:["Admin"]})
     vi.spyOn(global, 'fetch').mockImplementation(() => {
       return Promise.reject(new Error('T00 many m0nkey$'))
     })
@@ -170,7 +177,7 @@ describe('AdminAgencySelect', async () => {
   })
 
   it('displays error on server failure during update', async () => {
-    profile.set({name:"Ortho Stice", jwt:"some-token-value"})
+    profile.set({name:"Ortho Stice", jwt:"some-token-value", roles:["Admin"]})
 
     vi.spyOn(global, 'fetch').mockImplementation(() => {
       return Promise.resolve({ok: true, status:200, json: () => Promise.resolve({'total_count': 2, 'users':users}) })
