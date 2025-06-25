35ce5f7f0e647114c32d59faf795e6cfa4ec9493
Dan Felder <d.felder@kunzleigh.com>
2025-06-25 12:18:43 -0400

    Production Release
    Sprint 58 issues include:
    
    Dependabot Alert: tar-fs can extract outside the specified dir with a specific tarball #831
    BUG: Remove Ability to Access Training - Admin W/O Admin Permission #711
    BUG: Address Large Layout Shifts affecting Performance on the Training Report #729

---
+++ training-front-end/src/components/__tests__/AdminPanel.spec.js
@@ -0,0 +1,44 @@
+import {describe, expect, it} from "vitest";
+import {profile} from "../../stores/user.js";
+import {mount} from "@vue/test-utils";
+import AdminPanel from "../AdminPanel.vue";
+
+describe('AdminPanel', async () => {
+    
+    it('displays admin card links', async () => {
+        // Set the profile mock with Admin role
+        profile.set({ name: 'Amelia Sedley', jwt: 'some-token-value', roles: ['Admin'] });
+
+        const wrapper = await mount(AdminPanel, {
+            global: {
+                stubs: {
+                    // Stub child components that are not essential for the test
+                    USWDSAlert: true,
+                },
+            },
+        });
+        
+        const userMaintenanceLink = wrapper.find('div[id="user-maintenance-card"]')
+        const gspcLink = wrapper.find('div[id="gspc-card"]')
+        const systemReportsLink = wrapper.find('div[id="system-reports-card"]')
+        expect(userMaintenanceLink.exists()).toBe(true)
+        expect(gspcLink.exists()).toBe(true)
+        expect(systemReportsLink.exists()).toBe(true)
+    })
+
+    it('displays an error message when the user is not authorized', async () => {
+        // Change the profile to non-admin
+        profile.set({ name: 'Amelia Sedley', jwt: 'some-token-value', roles: [] });
+
+        const wrapper = await mount(AdminPanel, {
+            global: {
+                stubs: {
+                    // Stub child components that are not essential for the test
+                    USWDSAlert: true,
+                },
+            },
+        });
+
+        expect(wrapper.html()).toContain('You are not authorized to access.');
+    });
+})
