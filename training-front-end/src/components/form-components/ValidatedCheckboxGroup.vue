d39b0f78b8e4b906ba0181c0a1eedb4ec61c3019
DanFelder <d.felder@kunzliegh.com>
2025-08-20 10:40:26 -0400

    Production Release
    Sprint 62 issues included:
    
    Bug: Update Select All and Deselect All buttons on the Training Report set to 'Unstyled Default' #852
    Remove Fortify and Demand #853
    Add Admin Role to New User - Rickie America #856
    Remove Admin Role for User - Rickie America #861

--- training-front-end/src/components/form-components/ValidatedCheckboxGroup.vue
+++ training-front-end/src/components/form-components/ValidatedCheckboxGroup.vue
@@ -65,14 +65,14 @@ function handleCheckboxChange(){
       </span>
     </span>
     <button 
-      class="usa-button usa-button--hover margin-bottom-2"
+      class="usa-button margin-bottom-2"
       type="button"
       @click="checkAll"
     >
       Select All
     </button>
     <button 
-      class="usa-button usa-button--hover margin-bottom-2"
+      class="usa-button margin-bottom-2"
       type="button"
       @click="deselectAll"
     >
